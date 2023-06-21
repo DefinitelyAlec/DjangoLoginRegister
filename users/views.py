f40ff4f7f3e756f377d4dacd624f27af0b4a31e2
DefinitelyAlec <alecparent@rocketmail.com>
2023-06-20 18:45:20 -0600

    changed from basic HttpRequest home page to a template home page

--- users/views.py
+++ users/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 # Handles logic for the 'home' function
 # Currently returns a basic 'hello world' page
 def home(request):
-    return HttpResponse("<h1>Hello Worlds<h1>")
+    return render(request, 'users/home.html')
